61c6589a35400e61b5c7a4abdd3a74794122396c
segward <gustavhaverstad@gmail.com>
2025-09-15 07:32:17 +0200

    first commit

---
+++ asmqs.asm
@@ -0,0 +1,31 @@
+section .data
+    array db 1, 2, 3, 4, 5, 6, 7, 11, 15, 18
+    len equ $ - array
+    fmt db "%d", 10, 0
+
+section .text
+    global _main
+    extern _printf
+
+_main:
+    lea rbx, [rel array]      ; pointer to first element
+    mov rbp, 0                ; counter for printed elements
+
+.next:
+    cmp rbp, len              ; check if we printed 5 elements
+    je .done                  ; stop when value is 5
+
+    movzx rsi, byte [rbx]     ; load next element
+    lea rdi, [rel fmt]        ; format string
+    xor rax, rax
+    call _printf
+
+    inc rbx                   ; move to next element
+    inc rbp                   ; increment printed counter
+    jmp .next
+
+.done:
+    mov rax, 0x2000001        ; syscall exit
+    xor rdi, rdi
+    syscall
+
