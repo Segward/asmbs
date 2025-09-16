5b46637e3eaf969d96c0ae6eca3136327e968b4e
segward <gustavhaverstad@gmail.com>
2025-09-16 05:11:08 +0200

    functions with args

--- asmsort.asm
+++ asmsort.asm
@@ -1,23 +1,31 @@
 section .data
-  arr dd 1, 2, 3, 4, 5
-  len equ 5
+  arr1 dd 1, 2, 3, 4, 5
+  arr2 dd 2, 3, 4, 5, 6, 7
+  len1 equ 5
+  len2 equ 6
   msg db "%d", 10, 0
 
 section .bss
-  _sum resd 1
+  sum1 resd 1
+  sum2 resd 1
 
 section .text
   global _main
   extern _printf
   extern _exit
 
+sort:
+  ret
+
+; rbx points to array
+; rsi points to sum
+; r12 length
 sum:
   xor rbp, rbp
   xor rdx, rdx
-  lea rbx, [rel arr]
 
 .suml:
-  cmp rbp, len
+  cmp rbp, r12
   jge .sumd
   mov eax, [rbx + rbp*4]
   add edx, eax
@@ -25,23 +33,25 @@ sum:
   jmp .suml
 
 .sumd:
-  mov [rel _sum], edx
+  mov [rsi], edx
   ret
 
+; rsi points to sum
 sumout:
-  mov esi, dword [rel _sum]
+  mov esi, dword [rsi]
   lea rdi, [rel msg]
   xor rax, rax
   call _printf
   ret
 
+; rbx points to array
+; r12 length
 arrout:
   xor rbp, rbp
   xor rdx, rdx
-  lea rbx, [rel arr]
 
 .arroutl:
-  cmp rbp, len
+  cmp rbp, r12
   jge .arroutd
 
   mov esi, [rbx + rbp*4]
@@ -56,9 +66,17 @@ arrout:
   ret
 
 _main:
+  lea rsi, [rel sum1]
+  lea rbx, [rel arr1]
+  mov r12, len1
   call sum
   call sumout
-  call arrout
+
+  lea rsi, [rel sum2]
+  lea rbx, [rel arr2]
+  mov r12, len2
+  call sum
+  call sumout
 
   xor rdi, rdi
   call _exit
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-nasm -f macho64 asmbs.asm -o asmbs.o
-clang -target x86_64-apple-macos10.15 -o asmbs asmbs.o
+nasm -f macho64 asmsort.asm -o asmsort.o
+clang -target x86_64-apple-macos10.15 -o asmsort asmsort.o
 
