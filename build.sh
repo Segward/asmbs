028fd73750d454388b0518c73e0eacbe5f2cacf3
segward <gustavhaverstad@gmail.com>
2025-09-16 04:06:09 +0200

    Changed name to do bubble sort

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-nasm -f macho64 asmqs.asm -o asmqs.o
-clang -target x86_64-apple-macos10.15 -o asmqs asmqs.o
+nasm -f macho64 asmbs.asm -o asmbs.o
+clang -target x86_64-apple-macos10.15 -o asmbs asmbs.o
 
